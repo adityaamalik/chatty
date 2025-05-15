3a650a3800dd512ee58dd9b99d4acad84d08d21d
Varilla <44826959+adityaamalik@users.noreply.github.com>
2025-05-15 16:19:24 +0200

    fixing reg-ex issue with express 4

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
   const distPath = path.join(__dirname, "../../frontend/dist");
   app.use(express.static(distPath));
 
-  app.get("/:splat*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }

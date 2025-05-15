3c279b6bbe9bd7e2fe008e65a321df0fef2ce794
Varilla <44826959+adityaamalik@users.noreply.github.com>
2025-05-15 15:35:01 +0200

    fixing * issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
   const distPath = path.join(__dirname, "../../frontend/dist");
   app.use(express.static(distPath));
 
-  app.get("*", (req, res) => {
+  app.get("/:splat*", (req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
